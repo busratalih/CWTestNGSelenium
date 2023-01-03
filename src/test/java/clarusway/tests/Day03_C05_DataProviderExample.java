a2022ee73749b92656f58d5b843d4ed7778ec894
Büşra Talih <busra.talihhh@gmail.com>
2023-01-03 14:09:01 +0300

    03.01.2023

---
+++ src/test/java/clarusway/tests/Day03_C05_DataProviderExample.java
@@ -0,0 +1,16 @@
+package clarusway.tests;
+
+import clarusway.utilities.BaseTest;
+import org.testng.annotations.Test;
+
+public class Day03_C05_DataProviderExample extends BaseTest {
+
+    @Test
+
+    public void amazonSearchTest(){
+
+
+    }
+
+
+}
