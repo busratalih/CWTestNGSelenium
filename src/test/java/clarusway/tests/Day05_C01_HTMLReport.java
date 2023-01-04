824d90f34e6fb16e47bd075be5da29479a165acc
Büşra Talih <busra.talihhh@gmail.com>
2023-01-04 21:39:47 +0300

    04.01.2023

---
+++ src/test/java/clarusway/tests/Day05_C01_HTMLReport.java
@@ -0,0 +1,20 @@
+package clarusway.tests;
+
+import clarusway.utilities.BaseTestReport;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Day05_C01_HTMLReport extends BaseTestReport {
+
+    @Test
+    public void test01(){
+
+        System.out.println( "Test01 started at ");
+        driver.get("https://www.google.com/");
+        Assert.fail();
+    }
+
+
+
+
+}
