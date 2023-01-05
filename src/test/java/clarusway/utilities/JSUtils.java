5df793cb0eec354672d4bcc54367fcacf54a3cc6
Büşra Talih <busra.talihhh@gmail.com>
2023-01-05 22:17:22 +0300

    05.01.2023

--- src/test/java/clarusway/utilities/JSUtils.java
+++ src/test/java/clarusway/utilities/JSUtils.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 
 public class JSUtils {
 
-    protected static WebDriver driver;
+    public static WebDriver driver;
     //This method will takes two parameter: WebElement, and WebDriver
     //When you pass the element, JS will click on that element
     public static void clickElementByJS(WebElement element) {
